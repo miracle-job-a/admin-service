b4a83f36f4fe8d714bca2f55e0c1f92e1f27b0d5
kade <zsc0248@gmail.com>
2023-11-20 17:07:27 +0900

    토큰 필터 예외 컨트롤러 추가

---
+++ src/main/java/com/miracle/adminservice/controller/ErrorController.java
@@ -0,0 +1,14 @@
+package com.miracle.adminservice.controller;
+
+import com.miracle.adminservice.exception.UnauthorizedTokenException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ErrorController {
+
+    @RequestMapping("/errors/token")
+    public void errorToken() {
+        throw new UnauthorizedTokenException("토큰 값이 일치하지 않습니다.");
+    }
+}
