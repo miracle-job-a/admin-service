2c56b1e52c39c747484e5b219ab438bf2a253dce
kade <zsc0248@gmail.com>
2023-11-20 17:04:05 +0900

    feature / 전체 직무 조회, 직무 매칭 메서드 추가

--- src/main/java/com/miracle/adminservice/repsitory/JobRepository.java
+++ src/main/java/com/miracle/adminservice/repsitory/JobRepository.java
@@ -1,9 +1,19 @@
 package com.miracle.adminservice.repsitory;
 
+import com.miracle.adminservice.dto.response.JobResponseDto;
 import com.miracle.adminservice.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
+
+    @Query("SELECT new com.miracle.adminservice.dto.response.JobResponseDto(j.id, j.name) FROM Job j")
+    List<JobResponseDto> findAllJobs();
+
+    List<Job> findAllByIdIn(Set<Long> jobIdSet);
 }
