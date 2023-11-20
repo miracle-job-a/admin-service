c60fab64162b5e6626dc1341517a70282ba23659
kade <zsc0248@gmail.com>
2023-11-20 17:07:11 +0900

    토큰 인증 필터 추가

---
+++ src/main/java/com/miracle/adminservice/filter/TokenCheckFilter.java
@@ -0,0 +1,30 @@
+package com.miracle.adminservice.filter;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/v1/*")
+public class TokenCheckFilter implements Filter {
+
+    private final String privateKey = "TkwkdsladkdlrhdnjfrmqdhodlfjgrpaksgdlwnjTdjdy";
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        String id = httpServletRequest.getHeader("sessionId") + privateKey;
+        int miracle = id.hashCode();
+        int token = httpServletRequest.getIntHeader("miracle");
+
+        if (miracle != token) {
+            RequestDispatcher requestDispatcher = httpServletRequest.getRequestDispatcher("/errors/token");
+            requestDispatcher.forward(request,response);
+            return;
+        }
+        chain.doFilter(request, response);
+        }
+}
