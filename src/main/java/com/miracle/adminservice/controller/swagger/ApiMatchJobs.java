d19595f7cf56c2e732dd03e96042b4999f1cd4e1
kade <zsc0248@gmail.com>
2023-11-21 15:46:33 +0900

    feature / Swagger 명세위한 어노테이션 개발 (전체직무조회, 직무매칭, 전체스택조회, 스택매칭)

---
+++ src/main/java/com/miracle/adminservice/controller/swagger/ApiMatchJobs.java
@@ -0,0 +1,64 @@
+package com.miracle.adminservice.controller.swagger;
+
+import com.miracle.adminservice.dto.response.CommonApiResponse;
+import com.miracle.adminservice.dto.response.ErrorApiResponse;
+import com.miracle.adminservice.dto.response.SuccessApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Operation(summary = "직무 매칭", description = "Set으로 전달한 id의 직무 값을 매칭하여 반환합니다.",
+        responses = {
+                @ApiResponse(responseCode = "200",
+                        description = "정상 요청",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = {
+                                        @ExampleObject(
+                                                name = "성공",
+                                                value = "{\"httpStatus\": 200, \"message\": \"직무 매칭 성공\", \"data\": List<JobResponseDto> }"),
+                                },
+                                schema = @Schema(implementation = SuccessApiResponse.class)
+                        )),
+                @ApiResponse(responseCode = "400",
+                        description = "비정상 요청",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = @ExampleObject(
+                                        name = "Set is empty",
+                                        value = "{\"httpStatus\": 400, \"code\": \"400\", \"message\": \"직무 id Set에 값이 없습니다.\", \"data\": \"false\" }"
+                                ),
+                                schema = @Schema(implementation = CommonApiResponse.class)
+                        )),
+                @ApiResponse(responseCode = "401",
+                        description = "비정상 요청",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = @ExampleObject(
+                                        name = "토큰 인증 실패",
+                                        value = "{\"httpStatus\": 401, \"code\": \"401\", \"message\": \"토큰 값이 일치하지 않습니다.\", \"exception\": \"UnauthorizedTokenException\" }"
+                                ),
+                                schema = @Schema(implementation = ErrorApiResponse.class)
+                        )),
+                @ApiResponse(responseCode = "500",
+                        description = "서버 에러",
+                        content = @Content(
+                                mediaType = "application/json",
+                                examples = @ExampleObject(
+                                        name = "서버 에러",
+                                        value = "{\"httpStatus\": 500, \"code\": \"500\", \"message\": \"서버에 문제가 생겼습니다. 다시 시도해주세요.\", \"exception\": \"RuntimeException\" }"
+                                ),
+                                schema = @Schema(implementation = ErrorApiResponse.class)
+                        )),
+        })
+public @interface ApiMatchJobs {
+}
