515ebb973dbb44c2b6c07c583990cb968e84a739
kade <zsc0248@gmail.com>
2023-11-21 15:44:51 +0900

    ErrorController 매핑 변경

--- src/main/java/com/miracle/adminservice/controller/ErrorController.java
+++ src/main/java/com/miracle/adminservice/controller/ErrorController.java
@@ -1,13 +1,14 @@
 package com.miracle.adminservice.controller;
 
 import com.miracle.adminservice.exception.UnauthorizedTokenException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ErrorController {
 
-    @RequestMapping("/errors/token")
+    @GetMapping("/errors/token")
     public void errorToken() {
         throw new UnauthorizedTokenException("토큰 값이 일치하지 않습니다.");
     }
