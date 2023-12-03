87273c6dc3a108716b436019f5f7fb2b78f70b42
kade <zsc0248@gmail.com>
2023-12-03 17:07:39 +0900

    Feat: 유효성 검사 예외 추가

--- src/main/java/com/miracle/adminservice/exception/ExceptionController.java
+++ src/main/java/com/miracle/adminservice/exception/ExceptionController.java
@@ -8,12 +8,61 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptionController {
+
+    /**
+     *
+     * 400_1 유효성검사 - email
+     * 400_2 유효성검사 - name
+     * 400_3 유효성검사 - photo
+     * 400_4 유효성검사 - password
+     * 400_5 유효성검사 - bno
+     * 400_6 유효성검사 - ceoName
+     * 400_7 유효성검사 - sector
+     * 400_8 유효성검사 - address
+     * 400_9 유효성검사 - introduction
+     * 400_10 유효성검사 - employeeNum
+     * 400_11 유효성검사 - id
+     * 400_12 유효성검사 - question(faq)
+     * 400_13 유효성검사 - answer(faq)
+     * 400_14 유효성검사 - question(Question)
+     * 400_15 유효성검사 - title
+     * 400_16 유효성검사 - endDate
+     * 400_17 유효성검사 - tool
+     * 400_18 유효성검사 - workAddress
+     * 400_19 유효성검사 - postType
+     * 400_20 유효성검사 - testDate
+     * 400_21 유효성검사 - career
+     * 400_22 유효성검사 - mainTask
+     * 400_23 유효성검사 - workCondition
+     * 400_24 유효성검사 - process
+     * 400_25 유효성검사 - includeEnded
+     * 400_26 유효성검사 - stackIdSet
+     * 400_27 유효성검사 - jobIdSet
+     * 400_28 유효성검사 - addressSet
+     */
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public CommonApiResponse notValidHandle(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        log.info("[notValidHandle] : " + e.getMessage());
+        FieldError error = bindingResult.getFieldError();
+        String[] split = error.getDefaultMessage().split(":");
+
+        return ErrorApiResponse.builder()
+                .httpStatus(HttpStatus.BAD_REQUEST.value())
+                .code(split[0])
+                .message(split[1])
+                .exception("MethodArgumentNotValidException")
+                .build();
+    }
 
     /**
      * 401_1 토큰 인증
