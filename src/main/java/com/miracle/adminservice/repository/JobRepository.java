e833bbe80c2a93710e7a94e37745f7467c1316f8
kade <zsc0248@gmail.com>
2023-12-03 22:25:02 +0900

    Comment: 메서드 javadoc 추가

--- src/main/java/com/miracle/adminservice/repository/JobRepository.java
+++ src/main/java/com/miracle/adminservice/repository/JobRepository.java
@@ -1,6 +1,6 @@
 package com.miracle.adminservice.repository;
 
-import com.miracle.adminservice.dto.response.JobResponseDto;
+import com.miracle.adminservice.dto.response.StackAndJobResponseDto;
 import com.miracle.adminservice.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,10 +12,27 @@ import java.util.Set;
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
 
-    @Query("SELECT new com.miracle.adminservice.dto.response.JobResponseDto(j.id, j.name) FROM Job j")
-    List<JobResponseDto> findAllJobs();
+    /**
+     * @author kade
+     * @return List<StackAndJobResponseDto>
+     * 전체 직무를 반환합니다.
+     */
+    @Query("SELECT new com.miracle.adminservice.dto.response.StackAndJobResponseDto(j.id, j.name) FROM Job j")
+    List<StackAndJobResponseDto> findAllJobs();
 
+    /**
+     * @author kade
+     * @param jobIdSet
+     * @return List<Job>
+     * jobIdSet을 받고 해당하는 직무를 반환합니다.
+     */
     List<Job> findAllByIdIn(Set<Long> jobIdSet);
 
+    /**
+     * @author kade
+     * @param jobName
+     * @return Boolean
+     * jobName으로 존재 여부를 확인합니다.
+     */
     Boolean existsByName(String jobName);
 }
--- src/main/java/com/miracle/adminservice/repository/StackRepository.java
+++ src/main/java/com/miracle/adminservice/repository/StackRepository.java
@@ -1,6 +1,6 @@
 package com.miracle.adminservice.repository;
 
-import com.miracle.adminservice.dto.response.StackResponseDto;
+import com.miracle.adminservice.dto.response.StackAndJobResponseDto;
 import com.miracle.adminservice.entity.Stack;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,9 +12,27 @@ import java.util.Set;
 @Repository
 public interface StackRepository extends JpaRepository<Stack, Long> {
 
-    @Query("SELECT new com.miracle.adminservice.dto.response.StackResponseDto(s.id, s.name) FROM Stack s")
-    List<StackResponseDto> findAllStacks();
+    /**
+     * @author kade
+     * @return List<StackAndJobResponseDto>
+     * 전제 스택을 반환합니다.
+     */
+    @Query("SELECT new com.miracle.adminservice.dto.response.StackAndJobResponseDto(s.id, s.name) FROM Stack s")
+    List<StackAndJobResponseDto> findAllStacks();
+
+    /**
+     * @author kade
+     * @param stackIdSet
+     * @return List<Stack>
+     * stackIdSet을 받고 해당하는 stack을 반환합니다.
+     */
     List<Stack> findAllByIdIn(Set<Long> stackIdSet);
 
+    /**
+     * @author kade
+     * @param stackName
+     * @return Boolean
+     * stackName으로 존재 여부를 확인합니다.
+     */
     Boolean existsByName(String stackName);
 }
