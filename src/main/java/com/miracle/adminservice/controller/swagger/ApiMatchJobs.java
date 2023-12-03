2aa34eadae9742cfb498de9bae27c7c10ae8b821
kade <zsc0248@gmail.com>
2023-12-03 17:05:49 +0900

    Refactor: 공통 에러 명세 삭제

--- src/main/java/com/miracle/adminservice/controller/swagger/ApiMatchJobs.java
+++ src/main/java/com/miracle/adminservice/controller/swagger/ApiMatchJobs.java
@@ -39,26 +39,6 @@ import java.lang.annotation.Target;
                                 ),
                                 schema = @Schema(implementation = CommonApiResponse.class)
                         )),
-                @ApiResponse(responseCode = "401",
-                        description = "비정상 요청",
-                        content = @Content(
-                                mediaType = "application/json",
-                                examples = @ExampleObject(
-                                        name = "토큰 인증 실패",
-                                        value = "{\"httpStatus\": 401, \"code\": \"401\", \"message\": \"토큰 값이 일치하지 않습니다.\", \"exception\": \"UnauthorizedTokenException\" }"
-                                ),
-                                schema = @Schema(implementation = ErrorApiResponse.class)
-                        )),
-                @ApiResponse(responseCode = "500",
-                        description = "서버 에러",
-                        content = @Content(
-                                mediaType = "application/json",
-                                examples = @ExampleObject(
-                                        name = "서버 에러",
-                                        value = "{\"httpStatus\": 500, \"code\": \"500\", \"message\": \"서버에 문제가 생겼습니다. 다시 시도해주세요.\", \"exception\": \"RuntimeException\" }"
-                                ),
-                                schema = @Schema(implementation = ErrorApiResponse.class)
-                        )),
         })
 public @interface ApiMatchJobs {
 }
