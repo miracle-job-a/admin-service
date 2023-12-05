cdb65e72d592691e3f192dd032ee0b1babdcceb8
kade <zsc0248@gmail.com>
2023-12-05 16:05:14 +0900

    Feat: 유레카, 컨피그 적용 완료

--- src/main/java/com/miracle/adminservice/AdminServiceApplication.java
+++ src/main/java/com/miracle/adminservice/AdminServiceApplication.java
@@ -3,11 +3,13 @@ package com.miracle.adminservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
 @ServletComponentScan
+@EnableDiscoveryClient
 public class AdminServiceApplication {
 
 	public static void main(String[] args) {
