c9a52726bdc998177bcd8bd013771a82ee09449e
kade <zsc0248@gmail.com>
2023-12-16 22:44:36 +0900

    Feat: 토큰 인증 실패 경로 추가

--- src/main/java/com/miracle/adminservice/controller/ErrorController.java
+++ src/main/java/com/miracle/adminservice/controller/ErrorController.java
@@ -1,9 +1,9 @@
 package com.miracle.adminservice.controller;
 
 import com.miracle.adminservice.controller.swagger.ApiDefault;
+import com.miracle.adminservice.exception.InvalidTokenException;
 import com.miracle.adminservice.exception.UnauthorizedTokenException;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +13,10 @@ public class ErrorController {
     @GetMapping("/errors/token")
     public void errorToken() {
         throw new UnauthorizedTokenException("토큰 값이 일치하지 않습니다.");
+    }
+
+    @GetMapping("/invalid-token")
+    public void invalidToken() {
+        throw new InvalidTokenException("토큰 인증 실패");
     }
 }
